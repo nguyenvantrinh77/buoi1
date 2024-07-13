60614fde9af833b34fdc8c438b5d55c03ce3cb7f
Trinh <trinh040803@gmail.com>
2024-07-13 23:30:31 +0700

    cau 1:

---
+++ nhanhaisonguyenlon/nhan2songuyenlon.cs
@@ -0,0 +1,20 @@
+﻿using System.Numerics;
+
+namespace NhanHaiSoNguyenLon
+{
+    internal class Nhan2SoNguyenLon
+    {
+        static void Main()
+        {
+            Console.Write("Nhap so nguyen lon thu nhat: ");
+            BigInteger num1 = BigInteger.Parse(Console.ReadLine());
+
+            Console.Write("Nhap so nguyen lon thu hai: ");
+            BigInteger num2 = BigInteger.Parse(Console.ReadLine());
+
+            BigInteger result = num1 * num2;
+
+            Console.WriteLine($"\nKet qua cua phep nhan la: {result}");
+        }
+    }
+}
